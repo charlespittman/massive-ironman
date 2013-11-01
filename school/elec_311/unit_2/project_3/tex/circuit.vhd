c6e82abe63123c821ec15659173b2658049c745b
Charles Pittman <charles.pittman@gmail.com>
2013-11-01 18:33:36 -0400

    Clarifying comments

--- school/elec_311/unit_2/project_3/tex/circuit.vhd
+++ school/elec_311/unit_2/project_3/tex/circuit.vhd
@@ -26,22 +26,25 @@ architecture Structural of circuit is
 begin
 
   -- If SEL is active, give 1's complement.
+  -- ExclusiveOR port map(A, B, F)
   y3 : ExclusiveOR port map(SEL, Y(3), S(3));
   y2 : ExclusiveOR port map(SEL, Y(2), S(2));
   y1 : ExclusiveOR port map(SEL, Y(1), S(1));
   y0 : ExclusiveOR port map(SEL, Y(0), S(0));
 
   -- Add X, Y.  Note Cin of fa0 is SEL.  If active, finishes inverting Y from above.
+  -- FullAdder port map(A, B, Cin, Cout, Sum)
   fa3 : FullAdder port map(X(3), S(3), C(2), C(3), Z(3));
   fa2 : FullAdder port map(X(2), S(2), C(1), C(2), Z(2));
   fa1 : FullAdder port map(X(1), S(1), C(0), C(1), Z(1));
   fa0 : FullAdder port map(X(0), S(0), SEL, C(0), Z(0));
 
+  -- Cout is used to determine if overflow occurs, so a signal must be used in
+  -- the architecture.
+  Cout <= C(3);
+
   -- Overflow occurs when Cin != Cout in the last adder.
   -- (x != y) -> (xy + x'y')' -> (x+y)(x' + y') -> (x xor y)
   ovf : ExclusiveOR port map(C(2), C(3), OVR);
-
-  -- Needed to use Cout in logic above, so had to use a signal
-  Cout <= C(3);
 
 end Structural;
