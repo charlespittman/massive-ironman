02db1eec30a7715d30f7f3feeefecd9ab4935d3b
Charles <charles.pittman@gmail.com>
2014-03-09 19:10:53 -0400

    Added proper comments.

--- school/elec_330/homework/hw4_1.asm
+++ school/elec_330/homework/hw4_1.asm
@@ -1,42 +1,52 @@
-	Title "PIC18F452 IP5-6 Data Copy"
-	list p=18f452, f =inhx32
-	#include <p18f452.inc>		;header file
+	Title "hw4.1"
+	#include <p18f452.inc>
 
-buffer:         equ     0x20                ;results
-counter:        equ	0x01			;counter is reg01
+counter:        equ	0x00
+buffer:         equ     0x20    ;results
 
-	org	0x00			;reset vector
+        ;; Reset vector
+	org	0x00
 	goto	start
 
-	org	0x40
+;;; Array for source values.
+	org	0x20
+source:
+        db	0xe3, 0x72, 0x5d, 0x81, 0xcc, 0x34
+
+;;; Main program
+	org	0x30
 start:
-        movlw	0x00			;init PORTC as an output port
+        ;; Set PORTC as output port.
+        movlw	0x00
 	movwf	TRISC
+        ;; Set counter value.
 	movlw	0x06
 	movwf	counter
-	lfsr	FSR0,buffer		;init fsr0 pointer
-	movlw	upper source		;init table pointer
+        ;; Set file pointer to buffer.
+	lfsr	FSR0, buffer
+        ;; Set table pointer for source values
+	movlw	upper source
 	movwf	TBLPTRU
 	movlw	high source
 	movwf	TBLPTRH
 	movlw	low source
 	movwf	TBLPTRL
+
+;;; Copy next data byte to WREG.  Quit if counter lteq 0.
 next:
-	tblrd*+				;copy byte to table latch and inc table pointer
-	decf	counter,f		;decrement counter
-        bn      loaded                  ;counter goes negative if last item of source isn't copied to buffer
-	movf	TABLAT,w 		;copy byte to w
+	tblrd*+
+	decf	counter, f
+        bn      finished        ;counter goes negative if last item of source isn't copied to buffer
+	movf	TABLAT, w
+
+;;; If value is negative, copy to buffer.  Fetch next byte unless counter eq 0.
 	bnn     next
-	movwf	POSTINC0		;copy byte to data register and inc FSR0
-	bnz	next			;counter=0?
+	movwf	POSTINC0
+	bnz	next
 
-loaded:
-        movlw	0xff			;load completion indicator
-	movwf	PORTC			;turn on all leds at portc
+;;; Turn on the completion indicator before sleeping.
+finished:
+        movlw	0xff
+	movwf	PORTC
 	sleep
-
-	org	0x80			;data bytes
-source:
-        db	0xe3,0x72,0x5d,0x81,0xcc,0x34
-
 	end
--- school/elec_330/homework/hw4_2.asm
+++ school/elec_330/homework/hw4_2.asm
@@ -1,41 +1,44 @@
 	Title "hw4.2"
 	#include <p18f452.inc>
 
-counter:        equ     0x00
-buffer:         equ     0x01
+counter:        equ     0x00    ;number of positive values
+buffer:         equ     0x01    ;list of positive values
 
-	org	0x00			;reset vector
+        ;; Reset vector
+	org	0x00
 	goto	start
 
+;;; Array for source values.  0x00 represents last item.
         org     0x20
 source:
         db      0x66, 0x88, 0x75, 0xf2, 0xca, 0x00
 
+;;; Main program
 	org	0x30
 start:
-	lfsr	FSR0,buffer		;init pointer
-        movlw	upper source		;init table pointer
+        ;; Initialize file pointer to buffer.
+	lfsr	FSR0, buffer
+        ;; Initialize table pointer for source values.
+        movlw	upper source
 	movwf	TBLPTRU
 	movlw	high source
 	movwf	TBLPTRH
 	movlw	low source
 	movwf	TBLPTRL
 
+;;; Copy next data byte to WREG.  Quit if 0x00.
 fetch:
-        ;; Copy next data byte to WREG
         tblrd*+
-        movf    TABLAT,w
+        movf    TABLAT, w
         bz      finished            ;0x00 used to indicate final list item
 
+;;; If value is positive, increment counter and copy to buffer.  Fetch next byte.
 test:
-        ;; If value is positive, increment counter and copy to buffer.
-        ;; Get next byte
         bn      fetch
         incf    counter, f
-        movwf    POSTINC0
+        movwf   POSTINC0
         bra     fetch
 
 finished:
         sleep
-
         end
