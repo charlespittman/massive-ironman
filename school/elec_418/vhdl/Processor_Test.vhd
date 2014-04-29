b3527313a86cdde73d017e11531a45a0b297b2e4
Charles <charles.pittman@gmail.com>
2014-04-29 00:14:37 -0400

    Updated processor and test

---
+++ school/elec_418/vhdl/Processor_Test.vhd
@@ -0,0 +1,85 @@
+--------------------------------------------------------------------------------
+-- Company: The Citadel
+-- Engineer: Dr. Ron Hayne
+--
+-- Create Date:   14:40:15 02/26/2014
+-- Design Name:   
+-- Module Name:   C:/Hayne/ELEC418/Xilinx/Processor/Processor_Test.vhd
+-- Project Name:  Processor
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: PROCESSOR
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Processor_Test IS
+END Processor_Test;
+ 
+ARCHITECTURE behavior OF Processor_Test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT PROCESSOR
+    PORT(
+         CLK : IN  std_logic;
+         RESET : IN  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal CLK : std_logic := '0';
+   signal RESET : std_logic := '0';
+
+   -- Clock period definitions
+   constant CLK_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: PROCESSOR PORT MAP (
+          CLK => CLK,
+          RESET => RESET
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+     RESET <= '1';
+     wait for CLK_period*1.25;	
+     RESET <= '0';
+     wait for CLK_period*80;
+     wait;
+   end process;
+
+END;
