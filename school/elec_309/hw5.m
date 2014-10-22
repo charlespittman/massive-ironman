a12a00d8298958fd1086630fc1c6d873337efb29
Charles Pittman <charles.pittman@gmail.com>
2014-10-22 16:51:15 -0400

    hw5 plot

--- school/elec_309/hw5.m
+++ school/elec_309/hw5.m
@@ -10,13 +10,23 @@ pkg load signal
 # plot(t,x)
 # grid on;
 
-T = 5*(1/2);
-Fs = 1000;
-dt = 1/Fs;
-t = -10:dt:10;
-x = 4*sawtooth(pi*t);
-plot(t,x)
-grid on;
+# T = 5*(1/2);
+# Fs = 1000;
+# dt = 1/Fs;
+# t = -10:dt:10;
+# x = 4*sawtooth(pi*t);
+# plot(t,x)
+# grid on;
 
-#[pxx,f] = periodogram(x,[],length(x),Fs,'power');
-#plot(f,pxx)
+n = -10:10;
+Dn = 0.5 .* j.*(-2.*cos(2.*n) + (1./n).*sin(2.*n));
+
+magDn = abs(Dn);
+phaseDn = angle(Dn) * (180/pi)
+
+W = n.*(pi/2)
+
+subplot(2,1,1)
+plot(W,magDn)
+subplot(2,1,2)
+plot(W,phaseDn)
