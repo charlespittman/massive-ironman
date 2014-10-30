662e3c3096608ba76af5087c3a2b4ab1822b4452
charlespittman <charles.pittman@gmail.com>
2014-10-30 16:43:19 -0400

    Generating 2nd figure.

--- school/elec_309/hw/hw6.m
+++ school/elec_309/hw/hw6.m
@@ -1,72 +1,38 @@
-#I know that x=0 is NaN, so I need to manually put in values to the array (0.5 here)
-#Dn = 0.5 .* j.*(-2.*cos(2.*n) + (1./n).*sin(2.*n));
-#Dn(n==0) = 0.5
-
 n = -10:10;
 xn = [0 2 4 -4 -2 0 2 4 -4 -2 0 2 4 -4 -2 0 2 4 -4 -2 0];
 
 figure 1
+
 stem(n,xn)
 title("Original Signal")
 xlabel("Signal,x[n]")
 ylabel("Discrete Time, n")
 
+
 figure 2
 
-## This is a dumb way to do it.  Need to factor it into a for-loop
-x = 0;
-D0=(1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
-             xt(2) *  exp(-j*1.*x*(2*pi/5)) +
-             xt(3) *  exp(-j*2.*x*(2*pi/5)) +
-             xt(4) *  exp(-j*3.*x*(2*pi/5)) +
-             xt(5) *  exp(-j*4.*x*(2*pi/5)))
+N0 = 5;
 
-x = 1;
-D1=(1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
-             xt(2) *  exp(-j*1.*x*(2*pi/5)) +
-             xt(3) *  exp(-j*2.*x*(2*pi/5)) +
-             xt(4) *  exp(-j*3.*x*(2*pi/5)) +
-             xt(5) *  exp(-j*4.*x*(2*pi/5)))
+## subindexes have to be >0, which is why
+for x=0:10
+  D(x+1) = (1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
+                     xt(2) *  exp(-j*1.*x*(2*pi/5)) +
+                     xt(3) *  exp(-j*2.*x*(2*pi/5)) +
+                     xt(4) *  exp(-j*3.*x*(2*pi/5)) +
+                     xt(5) *  exp(-j*4.*x*(2*pi/5)))
+end
 
-x = 2;
-D2=(1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
-             xt(2) *  exp(-j*1.*x*(2*pi/5)) +
-             xt(3) *  exp(-j*2.*x*(2*pi/5)) +
-             xt(4) *  exp(-j*3.*x*(2*pi/5)) +
-             xt(5) *  exp(-j*4.*x*(2*pi/5)))
-
-x = 3;
-D3=(1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
-             xt(2) *  exp(-j*1.*x*(2*pi/5)) +
-             xt(3) *  exp(-j*2.*x*(2*pi/5)) +
-             xt(4) *  exp(-j*3.*x*(2*pi/5)) +
-             xt(5) *  exp(-j*4.*x*(2*pi/5)))
-
-x = 4;
-D4=(1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
-             xt(2) *  exp(-j*1.*x*(2*pi/5)) +
-             xt(3) *  exp(-j*2.*x*(2*pi/5)) +
-             xt(4) *  exp(-j*3.*x*(2*pi/5)) +
-             xt(5) *  exp(-j*4.*x*(2*pi/5)))
-
-#Dk = D0 + D1 + D2 + D3 + D4
-Dk = [D0 D1 D2 D3 D4]
+Dk = [D(1) D(2) D(3) D(4) D(5) D(6) D(7) D(8) D(9) D(10)]
 
 magDk = abs(Dk);
 phaseDk = angle(Dk) * (180/pi)
 
-#W = n.*(pi/2)
+subplot(2,1,1)
+plot(magDk)
+title("|Dk| vs. k")
+xlabel("k+1")
 
-#|Dn| vs omega
-#subplot(2,1,1)
-#plot(W,magDn)
-
-# Phase(Dn) vs. omega
-#subplot(2,1,2)
-#plot(W,phaseDn)
-
-plot(Dk)
-
-
-N0 = 5;
-k = 0:(N0-1)
+subplot(2,1,2)
+plot(phaseDk)
+title("phase(Dk) vs. k")
+xlabel("k+1")
