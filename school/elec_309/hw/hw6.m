e03e4c140dc7d982e4ee8f2a2dd827df902dc3f1
charlespittman <charles.pittman@gmail.com>
2014-10-30 16:45:44 -0400

    Was using old variable in function, so broke if run outside my workspace

--- school/elec_309/hw/hw6.m
+++ school/elec_309/hw/hw6.m
@@ -15,11 +15,11 @@ N0 = 5;
 
 ## subindexes have to be >0, which is why
 for x=0:10
-  D(x+1) = (1/N0) * (xt(1) *  exp(-j*0.*x*(2*pi/5)) +
-                     xt(2) *  exp(-j*1.*x*(2*pi/5)) +
-                     xt(3) *  exp(-j*2.*x*(2*pi/5)) +
-                     xt(4) *  exp(-j*3.*x*(2*pi/5)) +
-                     xt(5) *  exp(-j*4.*x*(2*pi/5)))
+  D(x+1) = (1/N0) * (xn(1) *  exp(-j*0.*x*(2*pi/5)) +
+                     xn(2) *  exp(-j*1.*x*(2*pi/5)) +
+                     xn(3) *  exp(-j*2.*x*(2*pi/5)) +
+                     xn(4) *  exp(-j*3.*x*(2*pi/5)) +
+                     xn(5) *  exp(-j*4.*x*(2*pi/5)))
 end
 
 Dk = [D(1) D(2) D(3) D(4) D(5) D(6) D(7) D(8) D(9) D(10)]
